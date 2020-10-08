f02f56c14d4844d7e71622928edd3918f1f191b9
imshaiknasir <skfiroj1994@gmail.com>
2020-10-09 02:25:46 +0530

    zen

--- Project02/ApiAutomation01.py
+++ Project02/ApiAutomation01.py
@@ -4,11 +4,19 @@ response = requests.get('http://216.10.245.166/Library/GetBook.php',
              params={'AuthorName':'Rahul Shetty2'},)
 
 print(response.text)
+#
+# outpt = response.json()
+# print(type(outpt))
+#
+# assert response.status_code == 200
+# print(response.headers)
+# assert response.headers['Content-Type'] == "application/json;charset=UTF-8"
 
-outpt = response.json()
-print(type(outpt))
+for book in response.json():
+    if book['isbn'] == "bcz533effed":
+        print(book)
+        break
 
-assert response.status_code == 200
-print(response.headers)
-assert response.headers['Content-Type'] == "application/json;charset=UTF-8"
+exBook = {'book_name': 'Learn Appium Automation with Java', 'isbn': 'bcz533effed', 'aisle': '20027'}
 
+assert book == exBook
---
+++ Project02/ApiAutomation02.py
@@ -0,0 +1,1 @@
+#Automating Post requests
