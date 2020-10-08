e9b658a282bae983bcab894d9a691e113682a95a
imshaiknasir <skfiroj1994@gmail.com>
2020-10-09 02:09:31 +0530

    zen

--- Project02/ApiAutomation01.py
+++ Project02/ApiAutomation01.py
@@ -4,3 +4,11 @@ response = requests.get('http://216.10.245.166/Library/GetBook.php',
              params={'AuthorName':'Rahul Shetty2'},)
 
 print(response.text)
+
+outpt = response.json()
+print(type(outpt))
+
+assert response.status_code == 200
+print(response.headers)
+assert response.headers['Content-Type'] == "application/json;charset=UTF-8"
+
